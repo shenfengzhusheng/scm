700e011b506269df3c660c5b911823b48f98a078
Jeken <xixingyingzhongdui@gmail.com>
2018-11-17 11:23:17 +0800

    121

---
+++ scm-study/src/test/java/org/xfs/test/study/SystemDemo.java
@@ -0,0 +1,9 @@
+package org.xfs.test.study;
+
+public class SystemDemo {
+
+    public static void main(String[]args){
+        String userDir=System.getProperty("user.dir");
+        System.out.println("userDir:"+userDir);
+    }
+}
